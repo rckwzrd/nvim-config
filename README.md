9d54d73f1745724505d015d2cfed4055a0bf591b
matt rhoads <mrhoads1@gmail.com>
2025-03-13 06:39:30 -0500

    update readme

--- README.md
+++ README.md
@@ -1,9 +1,26 @@
 # Neovim Configuration
 
-Now with structure!
+`nvim = v0.10.4`
+
+Now with structure and lazy plugins!
 
 TODO
 -[x] patch fonts
 - fix copy/paste :checkhealth
-- list of plugins
+-[ ] list of plugins
 - make sure nvim path in .basrc
+- config swap file
+
+Plugin Manager
+- Lazy
+
+Plugins
+- Neotree
+- Treesitter
+- Telescope
+- Vim-slueth
+- ...
+
+External Dependancies
+- Patched nerdfonts
+- Ripgrep
--- init.lua
+++ init.lua
@@ -40,39 +40,13 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   end
 end
 vim.opt.rtp:prepend(lazypath)
--- Set highlight on search
-vim.o.hlsearch = false
-
--- Make line numbers default
-vim.wo.number = true
-
--- Enable mouse mode
-vim.o.mouse = 'a'
-
--- Sync clipboard between OS and Neovim.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
-vim.o.clipboard = 'unnamedplus'
-
--- Enable break indent
-vim.o.breakindent = true
-
--- Save undo history
--- vim.o.undofile = true
-
--- Case-insensitive searching UNLESS \C or capital in search
-vim.o.ignorecase = true
-vim.o.smartcase = true
-
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
 
 local plugins = {
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
   { "tpope/vim-sleuth" },
   {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' }
+    "nvim-telescope/telescope.nvim", tag = "0.1.8",
+    dependencies = { "nvim-lua/plenary.nvim" }
   },
   {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
   {
