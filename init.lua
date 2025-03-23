97928b7749c251ac773782fc81bca0220cae2801
matt rhoads <mrhoads1@gmail.com>
2025-03-23 17:21:32 -0500

    added structured plugins

--- init.lua
+++ init.lua
@@ -7,8 +7,6 @@ vim.wo.number = true
 -- Enable mouse mode
 vim.o.mouse = 'a'
 
--- Sync clipboard between OS and Neovim.
---  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
 vim.o.clipboard = 'unnamedplus'
 
@@ -41,41 +39,8 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  { "tpope/vim-sleuth" },
-  {
-    "nvim-telescope/telescope.nvim", tag = "0.1.8",
-    dependencies = { "nvim-lua/plenary.nvim" }
-  },
-  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
-      "MunifTanjim/nui.nvim",
-    }
-  }
-}
-local opts = {}
+require("lazy").setup("plugins")
 
-require("lazy").setup(plugins, opts)
 
-local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 
-vim.keymap.set("n", "<leader>n", ":Neotree filesystem reveal right toggle<CR>")
-vim.keymap.set("n", "<leader>b", ":Neotree buffers reveal right toggle<CR>")
 
-local config = require("nvim-treesitter.configs")
-config.setup({
-  ensure_installed = { "lua", "python", "go", "sql" },
-  highlight = { enable = true },
-  indent = {enable = true }
-})
-
-require("catppuccin").setup()
-vim.cmd.colorscheme "catppuccin"
---
+++ lua/plugins/vim-slueth.lua
@@ -0,0 +1,4 @@
+-- Automagically set tabstop, tabwidth, and expandtab for a given file.
+return {
+  "tpope/vim-sleuth"
+}
